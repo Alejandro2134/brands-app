e541d79f9c5279a86e87094d1b93948c916f1745
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-08-24 21:56:39 -0500

    feat: cors added

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from src.infrastructure.api.routes.brands import router as brand_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(brand_router, prefix="/brand", tags=["Brands"])
